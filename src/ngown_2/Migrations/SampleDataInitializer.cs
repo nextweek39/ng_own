e42067d33d14d73b4d5639278228f298310eed7b
nextweek <maartri@gmail.com>
2016-04-12 17:42:57 +0800

    message

---
+++ src/ngown_2/Migrations/SampleDataInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+using ngown_2.Models;
+
+namespace ngown_2.Migrations
+{
+    public static class SampleDataInitializer
+    {
+ 
+        public static void EnsureSeedData(this ApplicationDbContext context)
+        {
+          
+                if (!context.Person.Any())
+                {
+
+
+                context.Person.AddRange(
+                    new Person { Name = "Mark Trinidad", Address = "Cebu" },
+                    new Person { Name = "Mark Trinidad", Address = "Manila" }
+                    );
+                       
+
+                    context.SaveChanges();
+                }
+
+            
+        }
+    }
+}
---
+++ src/ngown_2/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+
+using ngown_2.Models;
+namespace ngown_2.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Person>().HasKey(x => x.Id);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
